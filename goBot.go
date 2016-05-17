63d3ffbb58f46091aa4f722f765be3678007cd9c
Ionut Alexandru Isac <ionutalexandruisac@Ionuts-MacBook-Pro.local>
2016-05-17 20:58:31 +0100

    cleaner code

--- goBot.go
+++ goBot.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
-    // Variables to be defined
+    // IRC Variables to be defined (required for connecting the bot)
     var botVersion = "v0.1"
     var botNickname = "goBot"
     var botUsername = "goBot"
     var roomName = "#bx"
     var ircServer = "irc.freenode.net:6667"
+    var botMaster = "darks1de"
 
     // Trying to connect to server
     goBot := irc.IRC(botNickname, botUsername)
@@ -21,7 +22,7 @@ func main() {
         return
     }
 
-    // Message on join
+    // Message on join function
     goBot.Join(roomName)
     goBot.AddCallback("JOIN", func (e *irc.Event) {
       if e.Nick == botNickname {
@@ -31,18 +32,21 @@ func main() {
       }
     })
 
+    // Annoyingly repeat whatever the bot master types (test function - to be removed! ;-)
     goBot.AddCallback("PRIVMSG", func (e *irc.Event) {
-       goBot.Privmsg(roomName, e.Message())
+         if e.Nick == botMaster {
+              goBot.Privmsg(roomName, e.Message())
+         }
    })
 
    // Handle VERSION & PING events
    goBot.AddCallback("CTCP_VERSION", func(e *irc.Event) {
-	     goBot.Notice(e.Nick, "Currently running GoBot version: " + botVersion)
+	       goBot.Notice(e.Nick, "Currently running GoBot version: " + botVersion)
 	 })
    goBot.AddCallback("CTCP_PING", func(e *irc.Event) {
-       goBot.SendRawf("NOTICE %s :\x01%s\x01", e.Nick, e.Message())
+         goBot.SendRawf("NOTICE %s :\x01%s\x01", e.Nick, e.Message())
    })
 
-   goBot.Loop();
+         goBot.Loop();
 
 }
